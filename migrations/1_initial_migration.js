fb9676b738330c323de647802460a2d6b49acddc
hswick <hdswick@gmail.com>
2018-02-17 12:48:30 -0800

    Deployment fix added monitor test

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,16 +1,5 @@
 var Migrations = artifacts.require("./Migrations.sol");
-let ElectionSystem = artifacts.require('ElectionSystem')
-let ColoradoCoin = artifacts.require('ColoradoCoin')
-
-const fs = require('fs')
 
 module.exports = async function(deployer) {
   await deployer.deploy(Migrations);
-  await deployer.deploy(ElectionSystem)
-  await deployer.deploy(ColoradoCoin)
-
-  fs.writeFile('./config.json', JSON.stringify({
-    'electionSystemAddress': ElectionSystem.address,
-    'coloradoCoinAddress': ColoradoCoin.address
-  }), (err) => { if(err) console.error(err)})
 };
---
+++ test/monitorAgent.js
@@ -0,0 +1,79 @@
+const ElectionSystem = artifacts.require('./ElectionSystem.sol')
+const ColoradoCoin = artifacts.require('./ColoradoCoin.sol')
+const Web3 = require('web3')
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+const mineBlocks = require('./helpers/mineBlocks')(web3)
+
+describe('MonitoringAgent', async function () {
+  this.timeout(120000)
+
+  let electionSystem, accounts, electionId
+
+  before(async () => {
+
+    electionSystem = await ElectionSystem.at(ElectionSystem.address)
+    coloradoCoin = await ColoradoCoin.at(ColoradoCoin.address)
+
+    accounts = await web3.eth.getAccounts()
+
+    //send colorado coins to accounts
+    await coloradoCoin.transfer(accounts[1], 10000, {from: accounts[0]})
+    await coloradoCoin.transfer(accounts[2], 20000, {from: accounts[0]})
+  })
+
+  context('Test election system functionality with monitor', () => {
+
+    it('initializes new election', async () => {
+      const blockNumber = await web3.eth.getBlockNumber()
+      let tx = await electionSystem.initializeElection(blockNumber, blockNumber+100, blockNumber+120, 'eth denver hackers voting', coloradoCoin.address, {from: accounts[0]});
+
+      const result = tx.logs[1].args
+      
+      electionId = result.electionId
+    })
+
+    it('sends a vote', async () => {
+      const tx = await electionSystem.sendVote(electionId, true, {from: accounts[1]});
+
+      const result = tx.logs[0].args
+      assert.equal(result.voteId.toString(), electionId.toString())
+      assert.equal(result.voter, accounts[1].toLowerCase())
+
+    })
+
+    it('sends another vote', async () => {
+
+      let tx = await electionSystem.sendVote(electionId, false, {from: accounts[2]});
+
+      const result = tx.logs[0].args
+      assert.equal(result.voteId.toString(), electionId.toString())
+      assert.equal(result.voter, accounts[2].toLowerCase())
+
+    })
+
+    it('change voter balance', async () => {
+      const tx = await coloradoCoin.transfer(accounts[1], 10000, {from: accounts[0]})
+
+      const result = tx.logs[0].args
+
+      //await electionSystem.changeBalance(electionId, result._to, {from: accounts[3]})
+    })
+
+    it('change voter balance again', async () => {
+      const tx = await coloradoCoin.transfer(accounts[0], 10000, {from: accounts[1]})
+
+      const result = tx.logs[0].args
+
+      //await electionSystem.changeBalance(electionId, result._from, {from: accounts[3]})
+    })
+
+    it('get final election results', async () => {
+
+      await mineBlocks(120)
+
+      const electionResults = await electionSystem.getElectionResults(electionId)
+      assert.equal(electionResults[0].toNumber(), 10000)
+      assert.equal(electionResults[1].toNumber(), 20000)
+    })
+  })
+})
