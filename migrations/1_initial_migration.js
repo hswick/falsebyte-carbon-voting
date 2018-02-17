44852b19035ac9f63cd6327f00eca93e60c96574
Sami Mäkelä <mrsmkl@gmail.com>
2018-02-17 13:29:27 -0700

    travis

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,3 +1,4 @@
+
 var Migrations = artifacts.require("./Migrations.sol");
 let ElectionSystem = artifacts.require('ElectionSystem')
 let ColoradoCoin = artifacts.require('ColoradoCoin')
@@ -5,13 +6,10 @@ let ColoradoCoin = artifacts.require('ColoradoCoin')
 const fs = require('fs')
 
 module.exports = async function(deployer) {
-  console.log("startign thos")
   await deployer.deploy(Migrations);
   await deployer.deploy(ElectionSystem)
   await deployer.deploy(ColoradoCoin)
   
-  console.log("here doing something")
-
   fs.writeFile('./config.json', JSON.stringify({
     'electionSystemAddress': ElectionSystem.address,
     'coloradoCoinAddress': ColoradoCoin.address
