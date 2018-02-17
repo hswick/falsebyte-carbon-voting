e47689016097f930fd7a7cc95021bc0cd16861dd
Sami Mäkelä <mrsmkl@gmail.com>
2018-02-17 02:51:12 -0700

    working on making the vote contract implement aragon interface

--- contracts/ElectionSystem.sol
+++ contracts/ElectionSystem.sol
@@ -2,7 +2,23 @@ pragma solidity ^0.4.17;
 
 import './erc20.sol';
 
-contract ElectionSystem {
+import "@aragon/os/contracts/apps/AragonApp.sol";
+
+import "@aragon/os/contracts/common/IForwarder.sol";
+
+import "@aragon/os/contracts/lib/zeppelin/math/SafeMath.sol";
+import "@aragon/os/contracts/lib/misc/Migrations.sol";
+
+
+contract ElectionSystem is IForwarder, AragonApp {
+
+    uint256 constant public PCT_BASE = 10 ** 18;
+
+    bytes32 constant public CREATE_VOTES_ROLE = keccak256("CREATE_VOTES_ROLE");
+    bytes32 constant public MODIFY_QUORUM_ROLE = keccak256("MODIFY_QUORUM_ROLE");
+    
+    uint constant TALLY = 5;
+    
     struct Vote {
 	    // address voter;
 	    uint balance;
@@ -20,14 +36,75 @@ contract ElectionSystem {
         bytes32 description;
         ERC20 token;
         
+        string metadata;
+        bytes executionScript;
+        bool executed;
+        uint256 minAcceptQuorumPct;
+        uint startDate;
+        
     }
 
     mapping(bytes32 => Election) elections;
+    
+    event StartVote(uint256 indexed voteId);
+    event CastVote(uint256 indexed voteId, address indexed voter, bool supports, uint256 stake);
+    event ExecuteVote(uint256 indexed voteId);
+    event ChangeMinQuorum(uint256 minAcceptQuorumPct);
 
-    event NewElection(bytes32 id, address creator, uint startBlock, uint endBlock, uint tallyBlock, bytes32 description, address token);
+    ERC20 public token;
+    uint256 public supportRequiredPct;
+    uint256 public minAcceptQuorumPct;
+    uint64 public voteTime;
+    
+    function initialize(
+        ERC20 _token,
+        uint256 _supportRequiredPct,
+        uint256 _minAcceptQuorumPct,
+        uint64 _voteTime
+    ) onlyInit external
+    {
+        initialized();
 
-    function initializeElection(uint startBlock, uint endBlock, uint tallyBlock, bytes32 electionDescription, address token) public returns (bytes32) {
-        bytes32 id = keccak256(msg.sender, startBlock, endBlock, tallyBlock, electionDescription, token);
+        require(_supportRequiredPct > 1);
+        require(_supportRequiredPct <= PCT_BASE);
+        require(_supportRequiredPct >= _minAcceptQuorumPct);
+
+        token = _token;
+        supportRequiredPct = _supportRequiredPct;
+        minAcceptQuorumPct = _minAcceptQuorumPct;
+        voteTime = _voteTime;
+    }
+
+    /**
+    * @notice Change minimum acceptance quorum to `_minAcceptQuorumPct`
+    * @param _minAcceptQuorumPct New acceptance quorum
+    */
+    function changeMinAcceptQuorumPct(uint256 _minAcceptQuorumPct) authP(MODIFY_QUORUM_ROLE, arr(_minAcceptQuorumPct, minAcceptQuorumPct)) external {
+        require(supportRequiredPct >= _minAcceptQuorumPct);
+        minAcceptQuorumPct = _minAcceptQuorumPct;
+
+        ChangeMinQuorum(_minAcceptQuorumPct);
+    }
+    
+    function newVote(bytes _executionScript, string _metadata) auth(CREATE_VOTES_ROLE) external returns (uint256 voteId) {
+        bytes32 id = initializeElection(block.number, block.number+voteTime, block.number+voteTime+TALLY, 0, token);
+        Election storage el = elections[id];
+        el.executionScript = _executionScript;
+        el.metadata = _metadata;
+        
+        return uint(id);
+    }
+    
+    // will only be decided after tallying ends
+    function vote(uint256 _voteId, bool _supports, bool /* _executesIfDecided */) external {
+        // require(canVote(_voteId, msg.sender));
+        sendVote(bytes32(_voteId), _supports);
+    }
+
+    // event NewElection(bytes32 id, address creator, uint startBlock, uint endBlock, uint tallyBlock, bytes32 description, address token);
+    
+    function initializeElection(uint startBlock, uint endBlock, uint tallyBlock, bytes32 electionDescription, address _token) public returns (bytes32) {
+        bytes32 id = keccak256(msg.sender, startBlock, endBlock, tallyBlock, electionDescription, _token);
         require(endBlock > startBlock);
         require(tallyBlock > endBlock);
         require(startBlock > block.number);
@@ -37,9 +114,16 @@ contract ElectionSystem {
         election.votingEndBlockNumber = endBlock;
         election.tallyBlockNumber = tallyBlock;
         election.description = electionDescription;
-        election.token = ERC20(token);
-        NewElection(id, msg.sender, startBlock, endBlock, tallyBlock, electionDescription, token);
+        election.minAcceptQuorumPct = minAcceptQuorumPct;
+        election.token = ERC20(_token);
+        // NewElection(id, msg.sender, startBlock, endBlock, tallyBlock, electionDescription, _token);
+        StartVote(uint(id));
         return id;
+    }
+    
+    function canVote(uint256 _voteId, address _voter) public view returns (bool) {
+        Election storage el = elections[bytes32(_voteId)];
+        return el.votingStartBlockNumber <= block.number && el.votingEndBlockNumber >= block.number;
     }
 
     function sendVote(bytes32 electionId, bool vote) public {
@@ -52,6 +136,7 @@ contract ElectionSystem {
         el.votes[msg.sender] = Vote(balance, vote);
         if (vote) el.yesVoteTotal += balance;
         else el.noVoteTotal += balance;
+        CastVote(uint(electionId), msg.sender, vote, balance);
     }
 
     // should voter be able to change the vote
